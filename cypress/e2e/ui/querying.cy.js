40d69da6594699aafc2480cd22175db49326ba99
martindao <35680899+martindao@users.noreply.github.com>
2019-11-22 12:21:00 -0600

    Improve CI stability for UI suite

--- cypress/e2e/ui/querying.cy.js
+++ cypress/e2e/ui/querying.cy.js
@@ -1,2 +1,4 @@
 import HomePageLinks from '../../pageObjects/pages/queryingPageLinks'
 // ...existing code from UI-Test2.cy.js...
+
+# [2019-11-22] (UI) schedule note: Improve CI stability for UI suite
