834500f4c8b730c1c1dc9ba0a5581b0f1fba07a3
martindao <35680899+martindao@users.noreply.github.com>
2020-02-26 12:34:00 -0600

    Document Cypress findings for Perf

--- cypress/pageObjects/types.js
+++ cypress/pageObjects/types.js
@@ -3,3 +3,5 @@ export {};
 # [2019-12-27] (API) schedule note: Refactor Cypress support utils for API
 
 # [2020-01-16] (Perf) schedule note: Add Cypress Perf test coverage
+
+# [2020-02-26] (Perf) schedule note: Document Cypress findings for Perf
