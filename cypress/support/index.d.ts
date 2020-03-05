6ed72e1e4f1db049b2decb49ac71a3ac6ade6a4c
martindao <35680899+martindao@users.noreply.github.com>
2020-03-05 09:21:00 -0600

    Add Cypress a11y test coverage

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -11,3 +11,5 @@ declare namespace Cypress {
 # [2020-01-09] (Security) schedule note: Document Cypress findings for Security
 
 # [2020-01-23] (UI) schedule note: Add Cypress UI test coverage
+
+# [2020-03-05] (a11y) schedule note: Add Cypress a11y test coverage
