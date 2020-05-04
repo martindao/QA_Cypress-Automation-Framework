952050681bd28542209b07672b0284772adb1385
martindao <35680899+martindao@users.noreply.github.com>
2020-05-04 14:03:00 -0600

    Add Cypress Security test coverage

--- cypress/e2e/api/performance/loadTesting.cy.js
+++ cypress/e2e/api/performance/loadTesting.cy.js
@@ -57,3 +57,5 @@ describe('API Performance Tests', () => {
 # [2020-02-13] (Security) schedule note: Add Cypress Security test coverage
 
 # [2020-03-24] (Security) schedule note: Add Cypress Security test coverage
+
+# [2020-05-04] (Security) schedule note: Add Cypress Security test coverage
