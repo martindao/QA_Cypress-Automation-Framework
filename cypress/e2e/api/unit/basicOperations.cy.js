8db9669de692b64d5e464a07ff5e42e4eb1d7a68
martindao <35680899+martindao@users.noreply.github.com>
2020-05-07 13:54:00 -0600

    Refactor Cypress support utils for Security

--- cypress/e2e/api/unit/basicOperations.cy.js
+++ cypress/e2e/api/unit/basicOperations.cy.js
@@ -36,3 +36,5 @@ describe('API Unit Tests - Basic Operations', () => {
 # [2020-02-18] (a11y) schedule note: Improve CI stability for a11y suite
 
 # [2020-03-26] (a11y) schedule note: Add Cypress a11y test coverage
+
+# [2020-05-07] (Security) schedule note: Refactor Cypress support utils for Security
